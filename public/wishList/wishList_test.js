b8de7eddd6e9462bd2df55f66cd01120078da609
remus.arthur <remus.arthur@gmail.com>
2015-05-16 20:34:20 +0300

    WishList: add wishList test

---
+++ public/wishList/wishList_test.js
@@ -0,0 +1,16 @@
+'use strict';
+
+describe('myApp.wishList module', function() {
+
+  beforeEach(module('myApp.wishList'));
+
+  describe('WishList controller', function(){
+
+    it('should ....', inject(function($controller) {
+      //spec body
+      var view1Ctrl = $controller('WishListCtrl');
+      expect(view1Ctrl).toBeDefined();
+    }));
+
+  });
+});
