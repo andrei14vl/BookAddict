87c95b002fe787878848e7e8b7ea02f876361596
remus.arthur <remus.arthur@gmail.com>
2015-05-16 14:27:55 +0300

    add recommended books in view1

--- public/view1/view1.js
+++ public/view1/view1.js
@@ -10,5 +10,19 @@ angular.module('myApp.view1', ['ngRoute'])
 }])
 
 .controller('View1Ctrl', ['$scope', function($scope) {
-	$scope.mainText = "This is inserted text: partial for view 1.";
+	$scope.welcomeText = "Bine ai venit $utilizator !";
+
+	$scope.books = [
+	{'name': 'Vrajitorul din Oz',
+     'description': 'Descriere Oz.',
+     'link':'http://www.cineshop.ro/images/cache/T7/P/THE%20WIZARD%20OF%20OZ%20-%20BD.jpg'
+ 	},
+    {'name': 'Momente si schite',
+     'description': 'IL Caragiale este unul dintre cei mai apreciati autori.',
+     'link':'http://upload.wikimedia.org/wikipedia/commons/e/eb/Ion_Luca_Caragiale_-_Foto03.jpg'
+ 	},
+    {'name': 'Fight club™',
+     'description': 'Eliberare.',
+     'link':'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3C3s45NY4JpnDHMjUusowx2DfUiSPRkF0CGCpupkVNvBS-tzD'
+ 	}];
 }]);
