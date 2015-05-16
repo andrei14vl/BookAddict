294d7f80b366bc474c486efdadbdc7fe125aba2c
Costel <ec.dumea@gmail.com>
2015-05-16 14:59:55 +0300

    user routes v1

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var models  = require('../models');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+	var users = models.User.findAll();
+
+	//console.log("Users: "+JSON.stringify(users, null, 4));
+	res.json(users);
+});
+
+router.get('/user/:id', function(req, res, next) {
+  res.send('respond with a resource' . id);
 });
 
 module.exports = router;
