c8e9d1ee1651da8a80f67140d37fdc73f68ee51d
Costel <ec.dumea@gmail.com>
2015-05-23 16:47:54 +0300

    recomandation routes

--- routes/recomandation.js
+++ routes/recomandation.js
@@ -3,18 +3,44 @@ var router = express.Router();
 var models  = require('../models');
 
 
-router.get('/:id', function(req, res, next) {
-  	var users = models.User.findAll({
+
+router.get('/:id', function(req, res, next){
+	models.Book.findAll()
+		.then(function(books){
+			res.send(books);
+	}).catch(function(err){
+		res.send(404);
+	});
+});
+
+
+router.get('/test/:id', function(req, res, next){
+	var readBooks=models.User.findAll({
 		where:{
 			id: req.params.id
 		}
 	}).then(function(myUser){
+		console.log("usersusersuserruser");
+		console.log(myUser.Id);
+		var result = myUser[0].getRead().then(function(booksRead){
+			if(typeof result === "undefined")
+				res.send(0);
+			else{
+				var recomandBook = booksRead[0].Id;
+				console.log("sadasdasdasdas");
+				res.send(booksRead);
+			}
 
-			var result = myUser[0].getRead();
-			res.send(result);
 
+
+		}).catch(function(err){
+			res.send(err.Message);
+		});
+	}).catch(function(err){
+		res.send(err.Message);
 	});
 });
+
 
 
 module.exports = router;
