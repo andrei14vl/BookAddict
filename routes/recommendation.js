ca042ddf4c9a2d14a380ed2d66c67b0851ba1465
cristiana93 <tiugancristiana@gmail.com>
2015-05-25 10:53:32 +0300

    integrated genres in recommendations, but it's commented and untested.

--- routes/recommendation.js
+++ routes/recommendation.js
@@ -16,7 +16,72 @@ router.get('/', function(req, res, next){
 			immersiveStorylines: 0.2	
 		}
 	};
-	models.Book.findAll().then(function(books){
+/*
+	models.Genre.findAll({
+		attributes: ['id']
+	}).then(function(allGenres){
+		currentUser.getGenres({
+			attributes: ['id']
+		}).then(function(userGenres){
+			var genres=null;
+			if(userGenres===null)
+				genres=allGenres;
+			else
+				genres=userGenres;
+
+			models.Book.findAll({
+				where:{
+					genreId:{
+						$in: genres
+					}
+				}
+			}).then(function(books){
+				
+				models.Preference.find({
+					where:{
+						userId: currentUser.id
+					}
+				}).then(function(preferences){
+				
+					if(preferences == null)
+					{
+						preferences={
+							misteryAndSuspicion: 5,
+							beautifulLanguage: 5,
+							complexRelationships: 5,
+							intriguingCharacters: 5,
+							immersiveStorylines: 5
+						};
+					}
+					var recommend = knn(preferences, books, options);
+					
+					recommend.sort(function(a,b){
+						if (a.rating>b.rating)
+							return -1;
+						else if (a.rating < b.rating)
+							return 1;
+						else 
+							return 0;
+					});
+
+					res.send(recommend);
+				}).catch(function(err){
+					res.send(err.Message);
+				});
+			}).catch(function(err){
+				res.send(err.Message);
+			});
+
+		}).catch(function(err){
+			res.send(err.Message);
+		});		
+	}).catch(function(err){
+		res.send(err.Message);
+	})
+
+*/
+
+models.Book.findAll().then(function(books){
 
 		models.Preference.find({
 			where:{
@@ -52,12 +117,8 @@ router.get('/', function(req, res, next){
 	}).catch(function(err){
 		res.send(err.Message);
 	});
-	// models.Book.findAll()
-	// 	.then(function(books){
-	// 		res.send(books);
-	// }).catch(function(err){
-	// 	res.send(404);
-	// });
+
+
 });
 
 
