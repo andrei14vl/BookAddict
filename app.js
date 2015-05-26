d875e130b47670cecae5b93018660b85f5004f48
Ionut Balcau <balcau.ionut@gmail.com>
2015-05-26 22:03:42 +0300

    favicon.ico

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var favicon = require('express-favicon')
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 
